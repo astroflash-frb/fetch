29c8939903a0b00309cef6e26afc9567ea08833d
Franz Kirsten <franz.kirsten@chalmers.se>
2022-09-09 13:33:10 +0200

    removed yaml/json based conversion scripts -- conversions did not work

--- fetch/models/h_FT_DenseNet201_4_DMT_InceptionResNetV2_20_160/yaml2json.py
+++
@@ -1,6 +0,0 @@
-import yaml
-import json
-
-with open("ft_DenseNet201_4_dt_InceptionResNetV2_20_160.yaml","r") as yaml_in, open("ft_DenseNet201_4_dt_InceptionResNetV2_20_160.json","w") as json_out:
-	yaml_object = yaml.safe_load(yaml_in)
-	json.dump(yaml_object, json_out)
